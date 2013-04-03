c60ad241290d0468f0750a2ee601ce95e3240e2b
RageLtMan <rageltman [at] sempervictus>
2013-04-03 12:57:39 -0400

    XenAPI use SSL - resolves self signed cert bug
    Ruby's XMLRPC interface does not like self signed certificates.
    XMLRPC sessions can be monkeypatched on the fly to ignore SSL
    certificates before the initial connection attempt is made. With
    XenAPI, the session is created and connected in the same method.
    This approach resolves XenAPI upstream SSL validation as well.

--- lib/lab/controller/xenapi_controller.rb
+++ lib/lab/controller/xenapi_controller.rb
@@ -1,9 +1,5 @@
 require 'xenapi-ruby'
 
-# Ignore certs, this needs to get smarter
-# Goes into "use Rex for XMLRPC TODO list"
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
 module Lab
 module Controllers
 module XenApiController
@@ -64,4 +60,37 @@ module XenApiController
 
 end
 end
+end
+
+# Lets fix XenAPI's inability to deal with self-signed certs
+# Its actually an XMLRPC problem, not gem specific.
+
+class ::XenAPI::Session
+    def login_with_password(username, password, timeout = 1200,ssl_verify=false)
+      begin
+        @client = XMLRPC::Client.new2(@uri, nil, timeout)
+	if not ssl_verify
+	        @client.instance_variable_get(:@http).instance_variable_set(:@verify_mode, OpenSSL::SSL::VERIFY_NONE)
+	end
+        @session = @client.proxy("session")
+
+        response = @session.login_with_password(username, password)
+        raise XenAPI::ErrorFactory.create(*response['ErrorDescription']) unless response['Status'] == 'Success'
+
+        @key = response["Value"]
+
+        #Let's check if it is a working master. It's a small pog due to xen not working as we would like
+        self.pool.get_all
+
+        self
+      rescue Exception => exc 
+        error = XenAPI::ErrorFactory.wrap(exc)
+        if @block
+          # returns a new session
+          @block.call(error)
+        else
+          raise error
+        end 
+      end 
+    end
 end
